7a0b56e7dbc47115700cd35d6ed981b45244e34e
Faiz Khan <faizkhan00@gmail.com>
2016-06-09 04:39:00 -0500

    Install from pkg.bloqenterprise.net

--- ber-on-ubuntu/build_ber.sh
+++ ber-on-ubuntu/build_ber.sh
@@ -5,18 +5,15 @@ echo "This script will setup a Bloq Enterprise Router."
 echo "---"
 echo "To use this script you have to be using Ubuntu 14.04."
 echo "---"
-echo "Performing a general system update (this might take a while)..."		
+echo "Performing a general system update ..."		
+sudo apt-add-repository 'deb https://pkg.bloqenterprise/ stable main'
+gpg --recv-key 91955EB3D6410A98 && gpg -a --export 91955EB3D6410A98 | sudo apt-key add -
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y upgrade > /dev/null 2>&1
-sudo apt-get -y dist-upgrade > /dev/null 2>&1
-echo "---"
-echo "Installing prerequisites..."
-sudo apt-get -y install git build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev > /dev/null 2>&1
 echo "---"
 echo "Installing and configuring BER..."
-cd /tmp
-git clone https://gitlab.com/bloqinc/router.git && cd router/
-./autogen.sh && ./configure && make install
+sudo apt-get -y install router-bloq > /dev/null 2>&1
+bitcoind
 #TODO spawn in background and autosync
 #TODO install upstart script
 #TODO ensure logs are written to /var/log/
