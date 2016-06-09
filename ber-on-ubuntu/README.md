254e49edc53ab66637aaefca13f3ac8eac866e2a
Faiz Khan <faizkhan00@gmail.com>
2016-06-09 04:14:11 -0500

    README updates and cleanup of ber script

--- ber-on-ubuntu/README.md
+++ ber-on-ubuntu/README.md
@@ -1,7 +1,20 @@
-## Bloq Enterprise Router
 
 [![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Ffaiz-bloq%2Fazure-quickstart-templates%2Fber-on-ubuntu%2Fber-on-ubuntu%2Fazuredeploy.json)
 [![Visualize](http://armviz.io/visualizebutton.png)](http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Ffaiz-bloq%2Fazure-quickstart-templates%2Fber-on-ubuntu%2Fber-on-ubuntu%2Fazuredeploy.json)
 
-This template deploys the Bloq Enterprise Router.
+# Bloq Enterprise Router on Ubuntu
 
+This template quickly deploys a Bloq Enterprise Router onto Microsoft Azure. After configuring
+software the daemon will begin a sync with the network, which could take up to 24hr to fully sync.
+
+## Monitoring
+
+This deployment has a upstart script for enhanced monitoring.
+
+## Security
+
+This script enables boot scripts and firewall rules.
+
+## Trouble?
+
+Ping the author at faiz@bloq.com or support@bloq.com for more information.
--- ber-on-ubuntu/build_ber.sh
+++ ber-on-ubuntu/build_ber.sh
@@ -13,19 +13,11 @@ echo "---"
 echo "Installing prerequisites..."
 sudo apt-get -y install git build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev > /dev/null 2>&1
 echo "---"
-echo "Enabling Ubuntu's unattended security upgrades..."
-sudo apt-get -y install unattended-upgrades > /dev/null 2>&1
-echo 'APT::Periodic::Update-Package-Lists "1";' | sudo tee --append /etc/apt/apt.conf.d/20auto-upgrades > /dev/null 2>&1
-echo 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee --append /etc/apt/apt.conf.d/20auto-upgrades > /dev/null 2>&1
-echo "---"
-echo "Configuring the UFW firewall..."
-sudo ufw allow 22/tcp > /dev/null 2>&1
-sudo ufw allow 18080/tcp > /dev/null 2>&1
-sudo ufw --force enable > /dev/null 2>&1
-echo "---"
 echo "Installing and configuring BER..."
 cd /tmp
 git clone https://gitlab.com/bloqinc/router.git && cd router/
 ./autogen.sh && ./configure && make install
 #TODO spawn in background and autosync
+#TODO install upstart script
+#TODO ensure logs are written to /var/log/
 echo '...done.'
