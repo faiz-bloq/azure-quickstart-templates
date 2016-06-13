aa4951166053f5bbaeb25cae08eb7fb4a0979c36
Faiz Khan <faizkhan00@gmail.com>
2016-06-13 09:04:21 -0500

    Add upstart script

--- ber-on-ubuntu/build_ber.sh
+++ ber-on-ubuntu/build_ber.sh
@@ -5,16 +5,24 @@ echo "This script will setup a Bloq Enterprise Router."
 echo "---"
 echo "To use this script you have to be using Ubuntu 14.04."
 echo "---"
-echo "Performing a general system update ..."		
+echo "Performing a general system update ..."   
 sudo apt-add-repository 'deb https://pkg.bloqenterprise/ stable main'
 gpg --recv-key 91955EB3D6410A98 && gpg -a --export 91955EB3D6410A98 | sudo apt-key add -
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y upgrade > /dev/null 2>&1
 echo "---"
 echo "Installing and configuring BER..."
+# Add upstart task
+cat << EOF > /etc/init/ber.conf
+    description "BER Node Service"
+
+    start on starting
+    script
+      /usr/local/bin/bitcoind
+    end script
+    EOF
+
+chmod +x /etc/init/ber.conf
+sudo service ber start
 sudo apt-get -y install router-bloq > /dev/null 2>&1
-bitcoind
-#TODO spawn in background and autosync
-#TODO install upstart script
-#TODO ensure logs are written to /var/log/
-echo '...done.'
+echo 'BER started, enabled, installed...done.'
