3ce403780b57477fcc25556b3c9d10bff248df79
Faiz Khan <faizkhan00@gmail.com>
2016-06-13 09:17:19 -0500

    General cleanup

--- ber-on-ubuntu/build_ber.sh
+++ ber-on-ubuntu/build_ber.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 clear
-echo "This script will setup a Bloq Enterprise Router."
-echo "---"
-echo "To use this script you have to be using Ubuntu 14.04."
-echo "---"
-echo "Performing a general system update ..."   
+echo "This script will setup a Bloq Enterprise Router.\n"
+
+echo "To use this script you have to be using Ubuntu 14.04.\n"
+
+echo "Adding Bloq Enterprise Repo...\n"   
 sudo apt-add-repository 'deb https://pkg.bloqenterprise/ stable main'
-gpg --recv-key 91955EB3D6410A98 && gpg -a --export 91955EB3D6410A98 | sudo apt-key add -
+
+echo "Receiving GPG keys...\n"
+sudo gpg --recv-key 91955EB3D6410A98 
+sudo gpg -a --export 91955EB3D6410A98 | sudo apt-key add -
+
+echo "Performing update...\n"
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y upgrade > /dev/null 2>&1
-echo "---"
-echo "Installing and configuring BER..."
-# Add upstart task
+
+echo "Installing and configuring BER...\n"
+sudo apt-get -y install router-bloq > /dev/null 2>&1
+
+echo "Configuring upstart ...\n"
 cat << EOF > /etc/init/ber.conf
     description "BER Node Service"
 
@@ -20,9 +27,8 @@ cat << EOF > /etc/init/ber.conf
     script
       /usr/local/bin/bitcoind
     end script
-    EOF
-
+EOF
 chmod +x /etc/init/ber.conf
 sudo service ber start
-sudo apt-get -y install router-bloq > /dev/null 2>&1
+
 echo 'BER started, enabled, installed...done.'
